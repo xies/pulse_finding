9a5633f09cd0163bb52245e09ec71ebbb08f1763
xie.shicong@gmail.com <xie.shicong@gmail.com@yourdomain.com>
2013-08-13 19:12:40 +0000

    Finished make_embryo_pulse_movie.m

--- pulse_analysis/pulse_visualization/make_embryo_pulse_movie.m
+++ pulse_analysis/pulse_visualization/make_embryo_pulse_movie.m
@@ -1,33 +1,70 @@
 function F = make_embryo_pulse_movie(fits,cells,input,embryoID)
+%MAKE_EMBRYO_PULSE_MOVIE - Make a movie of all pulses found within
+% an embryo. Only the center frames of each pulse is highlighted,
+% and the fitID and cellID of that pulse will be shown in text.
+%
+% SYNOPSIS:
+
 
 if nargin > 3
     fits = fits.get_embryoID(embryoID);
     cells = cells.get_embryoID(embryoID);
 end
 
+% check that all fits and cells belong to the same embryo
+if numel(unique([fits.embryoID cells.embryoID])) > 1
+	error('All CellObj and Fitted must belong to the same embryo.');
+end
+
+% get non-NaN times
 dev_time = cells(1).dev_time;
 dev_time = dev_time( ~isnan(dev_time) );
-stackIDs = cat(1,fits.stackID);
+
+% get the central frame of a pulse
 center_frames = cellfun(@(x) fix(mean(x)),{fits.width_frames});
+% get fitIDs + cellIDs for drawing later
+fIDs = [fits.fitID];
+cIDs = [fits.cellID];
 
-num_cells = numel(cells);
 num_frames = numel(dev_time);
-
 X = input.X; Y = input.Y;
+
 
 for frame = 1:num_frames
     
-    
-    m = zeros(1,num_cells);
-    m(cIDs((center_frames == frame))) = 1;
-    for i = 1:num_cells
-        
-        mask = make_cell_mask(cells,frame,cells(i).stackID,input);
-        this_frame(mask) = m(frame);
-        
+	this_img = zeros(Y,X);
+	% Find all highlighted cells and create a mask
+	idx = find(center_frames == frame);
+	num_on = numel(idx);
+	on_cells = cIDs(idx);
+	on_fits = fIDs(idx);
+
+	x = zeros(1,num_on);
+	y = zeros(1,num_on);
+	str_cellID = cell(1,num_on);
+	str_fitID = cell(1,num_on);
+    for i = 1:num_on
+
+		% create mask
+        mask = cells(on_cells(i)).make_mask(frame,input);
+        this_img = this_img + double(mask);
+
+		% grab centroids for plotting
+		cx = cells(on_cells(i)).centroid_x; cy = cells(on_cells(i)).centroid_y;
+		cx = cx(frame,:)/input.um_per_px; cy = cy(frame,:)/input.um_per_px;
+		x(i) = cx; y(i) = cy;
+		% grab fitID + cellID
+		str_cellID{i} = on_cells(i);
+		str_fitID{i} = on_fits(i);
     end
+
+    imshow(this_img,[]);
+	hold on;
+	text(x-10,y+8,str_fitID,'Color','red','fontsize',10);
+    text(x-10,y-8,str_cellID,'Color','green','fontsize',10);
+    hold off;
     
-    
+    F(frame) = getframe;
     
 end
 
