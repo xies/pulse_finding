de879b72e845ef5f4b57c9674f4a6377e0dcd3f7
xie.shicong@gmail.com <xie.shicong@gmail.com@yourdomain.com>
2013-08-13 16:35:25 +0000

    iseven.m -> fixed. working on make_embryo_pulse_movie.m

--- pulse_analysis/pulse_visualization/make_embryo_pulse_movie.m
+++ pulse_analysis/pulse_visualization/make_embryo_pulse_movie.m
@@ -1,3 +1,34 @@
-function make_embryo_pulse_movie(fits,cells)
+function F = make_embryo_pulse_movie(fits,cells,input,embryoID)
+
+if nargin > 3
+    fits = fits.get_embryoID(embryoID);
+    cells = cells.get_embryoID(embryoID);
+end
+
+dev_time = cells(1).dev_time;
+dev_time = dev_time( ~isnan(dev_time) );
+stackIDs = cat(1,fits.stackID);
+center_frames = cellfun(@(x) fix(mean(x)),{fits.width_frames});
+
+num_cells = numel(cells);
+num_frames = numel(dev_time);
+
+X = input.X; Y = input.Y;
+
+for frame = 1:num_frames
+    
+    
+    m = zeros(1,num_cells);
+    m(cIDs((center_frames == frame))) = 1;
+    for i = 1:num_cells
+        
+        mask = make_cell_mask(cells,frame,cells(i).stackID,input);
+        this_frame(mask) = m(frame);
+        
+    end
+    
+    
+    
+end
 
 end
