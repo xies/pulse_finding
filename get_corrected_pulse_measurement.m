b9f4e5a42289b2666e1ad9a66c31095d2c788535
xie.shicong@gmail.com <xie.shicong@gmail.com@yourdomain.com>
2014-01-17 18:03:20 +0000

    Moved around stuff

--- get_corrected_pulse_measurement.m
+++
@@ -1,7 +0,0 @@
-function M = get_corrected_pulse_measurement(fits,meas,input,fit_opts)
-
-fits = fits.align_fits(meas,'measurement',fit_opts);
-fits = fits.resample_traces('measurement',[input.dt],fit_opts);
-M = cat(1,fits.corrected_measurement);
-
-end
--- pulse_tracking/track_pulse_script.m
+++ pulse_tracking/track_pulse_script.m
@@ -1,27 +1,39 @@
 %%TRACK_PULSE_SCRIPT Pipeline for
 
-%% Load MDF into matrix
+%%
 
-mdf_file = '~/Desktop/Tracked pulses/01-30-2012-4/01-30-2012-4-merged_acm.tif.mdf'; embryoID = 1;
-% mdf_file = '~/Desktop/Tracked pulses/10-25-2012-1/10-25-2012-1-acm.mdf'; embryoID = 4;
-% mdf_file = '~/Desktop/Tracked pulses/01-30-2012-7/01-30-2012-7_acm.mdf'; embryoID = 2;
+mdf_file{1} = '~/Desktop/Tracked pulses/01-30-2012-4/01-30-2012-4-merged_acm.tif.mdf'; embryoID(1) = 1;
+mdf_file{2} = '~/Desktop/Tracked pulses/01-30-2012-7/01-30-2012-7_acm.mdf'; embryoID(2) = 2;
+mdf_file{3} = '~/Desktop/Tracked pulses/10-15-2012-1/10-15-2012-1.mdf'; embryoID(3) = 3;
+mdf_file{4} = '~/Desktop/Tracked pulses/10-25-2012-1/10-25-2012-1-acm.mdf'; embryoID(4) = 4;
+mdf_file{5} = '~/Desktop/Tracked pulses/11-07-2012-1/11-07-2012-1_acm.mdf'; embryoID(5) = 5;
+mdf_file{6} = '~/Desktop/Tracked pulses/Twist injection series 006/twist_series_006_mimi.mdf'; embryoID(6) = 6;
+mdf_file{7} = '~/Desktop/Tracked pulses/Twist injection series 022/twist_injection_022_mimi.mdf'; embryoID(7) = 7;
+mdf_file{8} = '~/Desktop/Tracked pulses/12-04-2013 twi RNAi/merge_t1-70.mdf'; embryoID(8) = 8;
 
-mdf_mat = read_mdf(mdf_file);
+% mdf_file{1} = '~/Desktop/Tracked pulses/Twist injection series 006/twist_series_006_mimi.mdf'; embryoID(1) = 1;
+% mdf_file{2} = '~/Desktop/Tracked pulses/Twist injection series 022/twist_injection_022_mimi.mdf'; embryoID(2) = 2;
+% mdf_file{4} = '~/Desktop/Tracked pulses/Control Injection Series 002/control002.mdf'; embryoID(4) = 4;
+match_thresh = 1;
 
-[tracks,cells] = load_mdf_track(mdf_mat, embryo_stack, embryoID, 1, cells);
-
-%% Perform matching to fitted pulses
-
-clear pulse
-
-for i = 1:1
-    match_thresh = 1;
+for i = 8
     
-    pulse(i) = Pulse(tracks,mdf_file,fits,fit_opts(embryoID),cells);
+    % Load MDF into matrix
+    mdf_mat = read_mdf(mdf_file{i});
+    [tracks,cells_raw] = load_mdf_track(mdf_mat, embryo_stack, embryoID(i), 1, cells_raw);
+
+    % Perform matching to fitted pulses
+    
+    pulse(i) = Pulse(tracks,mdf_file{i},fits_raw,fit_opts,cells_raw);
     pulse(i) = pulse(i).match_pulse(match_thresh);
     pulse(i) = pulse(i).categorize_mapping;
     
-    pulse(i).embryoID = embryoID;
+    pulse(i).embryoID = embryoID(i);
+    display(['EmbryoID: ' num2str(i)]);
+    display(mdf_file{i});
     display(pulse(i));
 
-end
+end 
+
+fits_curated = [pulse.fits];
+cells_curated = [pulse.cells];
