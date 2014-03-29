2fcf2727b73de2e67eb1d8defb4d663d52303e18
xie.shicong@gmail.com <xie.shicong@gmail.com@9b36e402-b81e-7e89-c4c8-0bfeb8bcc5cf>
2014-03-29 17:42:14 +0000

    Finished comments in stpp_wrapper.R

--- stpp_wrapper.R
+++ stpp_wrapper.R
@@ -24,13 +24,16 @@ get_PCFhat_stpp <- function( xyt, s.region=NULL, t.region=NULL, u, v,
 	mut = Mt$y[ findInterval( t, Mt$x) ] * dim(xyt)[1]
 	
 	# Estimate 2d spatial kernel density
-	# Optimize the kernel bandwidth (h) by mean squared-error (MSE)
-	#	analysis at set h values to scan
-	#	Default: number of h-values: 50, max h-value: 4
-	
+	# Find the optimal spatial kernel size by scanning through set
+	# intervals of hs and finding the mean-squared error minimizing
+	# value. (Could be an underestimate of optimal hs)
+	#	Default behavior: number of h-values: 50, max h-value: 4	
 	hmsemin = mse2d( as.points(xy), s.region, nsmse = 50, range = 10)
 	hmsemin = hmsemin$h[ which.min(hmsemin$mse) ] # Argmin MSE
 	
+	# Construct the 2D kernel estimate density via kernel2d
+	#	Find abcissa in the KDE and construct mhat, the estimate of
+	#	spatial KDE error
 	Ms = kernel2d( as.points(xy), s.region, h=h, nx=nx, ny=ny)
 	atx = findInterval( x=xyt[,1], vec = Ms$x)
 	aty = findInterval( x=xyt[,2], vec = Ms$y)
@@ -38,7 +41,10 @@ get_PCFhat_stpp <- function( xyt, s.region=NULL, t.region=NULL, u, v,
 	for(i in 1:length(atx)) {
 		mhat = c(mhat, Ms$z[atx[i],aty[i]])
 	}
-	
+
+	# Use PCFhat_label - will call kernel_pcf_embryos.f if label is NULL
+	# and kernel_pcf_embryos_labels.f if label is given
+	# Both are slight modifications of original STPP pcfhat script	
 	g = PCFhat_label(xyt=xyt,
 				lambda = mhat*mut/ dim(xyt)[1],
 				dist = u, times = v,
