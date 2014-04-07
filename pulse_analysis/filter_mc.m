2bc82b486d7d08aa9b1aeea65724b8d5b8958c05
xies <xies@mit.edu>
2014-04-07 15:37:15 -0400

    Filters MC struct by logical index

---
+++ pulse_analysis/filter_mc.m
@@ -0,0 +1,26 @@
+function MC = filter_mc(MC,I)
+
+fieldnames = fields(MC.empirical);
+fieldnames= setdiff(fieldnames,{'neighbor_windows'});
+
+for i = 1:numel(fieldnames)
+    if isvector(MC.empirical.(fieldnames{i}))
+        MC.empirical.(fieldnames{i}) = ensure_column(MC.empirical.(fieldnames{i}));
+    end
+    MC.empirical.(fieldnames{i}) = ...
+        MC.empirical.(fieldnames{i})(I,:);
+end
+
+N = numel(MC.random_cell);
+for n = 1:N
+    
+    for i = 1:numel(fieldnames)
+        if isvector(MC.random_cell(n).(fieldnames{i}))
+            MC.random_cell(n).(fieldnames{i}) = ensure_column(MC.random_cell(n).(fieldnames{i}));
+        end
+        MC.random_cell(n).(fieldnames{i}) = ...
+            MC.random_cell(n).(fieldnames{i})(I,:);
+    end
+    
+end
+end
