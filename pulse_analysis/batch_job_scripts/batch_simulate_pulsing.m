818656143e026205b9ac6c216980b5ec998b5a9c
Shicong Xie <xies@mit.edu>
2014-06-17 22:40:42 -0400

    Added .sh submittion script generator and functional .m script

--- pulse_analysis/batch_job_scripts/batch_simulate_pulsing.m
+++ pulse_analysis/batch_job_scripts/batch_simulate_pulsing.m
@@ -1,55 +1,51 @@
 %BATCH_SIMULATE_PULSING
 % To be used from a master shell script for use in batch jobs.
 
+function batch_simulate_pulsing(INPUT_MAT_FILE,iterID)
+
+addpath(genpath('~/Code'))
+
+[DIR_STR,name,ext] = fileparts(INPUT_MAT_FILE);
+
+if ~strcmpi(ext,'mat'),
+	error('Needs a .mat file as input.');
+end
+
+disp(['Loading input pulse file: ' INPUT_MAT_FILE]);
+load(INPUT_MAT_FILE);
+
+% Estimate pulsing parameters
 [f,nC] = estimate_simulation_params(fits,cells);
 
-% Perform permultation analysis
+disp('Performing pulsing simulation...');
+[fits_bs,cells_bs] = ...
+	fits.simulate_pulsing(cells,f);
 
-for n = 1:50
+fitsOI = fits_bs.get_embryoID(1:5);
+cellsOI = cells_bs.get_embryoID(1:5);
 
-    % generate randomized cells
-    name = 'wt';
+[fits_bs,cells_bs] = fitsOI.simulate_pulsing(cellsOI,f,nC);
 
-    fitsOI = fits.get_embryoID(1:5);
-    cellsOI = cells.get_embryoID(1:5);
+fitsOI = fits_bs.get_embryoID(1:5);
+cellsOI = cells_bs.get_embryoID(1:5);
 
-    [fits_bs,cells_bs] = fitsOI.simulate_pulsing(cellsOI,f,nC);
+time_windows = 10:10:100; % seconds
 
-    fitsOI = fits_bs.get_embryoID(1:5);
-    cellsOI = cells_bs.get_embryoID(1:5);
+neighbor_defition.temporal.def = @(time_diff,tau) (time_diff < tau & time_diff > 0);
+neighbor_defition.temporal.windows = time_windows;
+neighbor_defition.spatial.def = 'identity';
 
-    time_windows = 10:10:100; % seconds
-    
-    clear neighbor_definition
-    neighbor_defition.temporal.def = @(time_diff,tau) (time_diff < tau & time_diff > 0);
-    neighbor_defition.temporal.windows = time_windows;
-    neighbor_defition.spatial.def = 'identity';
+fitsOI = fitsOI.find_near_fits(cellsOI,neighbor_defition);
 
-    fitsOI = fitsOI.find_near_fits(cellsOI,neighbor_defition);
+o.Nboot = 50;
+o.timewindows = time_windows;
+o.neighbor_def = neighbor_defition;
+o.monte_carlo = 'permute';
+o.filter = 'on';
 
-    nearIDs = cat(1,fitsOI.nearIDs);
-    near_angles = cat(1,fitsOI.near_angles);
+o.savepath = [DIR_STR]
 
-    % Convert to number of pulses
-    num_near = cellfun(@(x) numel(x(~isnan(x))), nearIDs);
-
-    % Generates permutation cells
-    entries = {'Ratcheted (stereotyped)','Ratcheted (weak)','Ratcheted (delayed)','Un-ratcheted','Stretched'};
-
-    clear o
-    o.Nboot = 50;
-    o.timewindows = time_windows;
-    o.neighbor_def = neighbor_defition;
-    o.monte_carlo = 'permute';
-    o.filter = 'on';
-
-    % o.savepath = [];
-    o.savepath = ...
-        ['~/Desktop/simulated pulses/mc_stackID_', name, '_', ...
-        'iter_', num2str(n), '_', neighbor_defition.spatial.def, ...
-        '_Nboot', num2str(o.Nboot), '_', o.monte_carlo, '_neighborfilt_', o.filter, ...
-        '_k' num2str(num_clusters)];
-
-    MC_wt_sim{n} = monte_carlo_pulse_location(fitsOI,cellsOI, o);
+disp('Permutation analysis...')
+MC = monte_carlo_pulse_location(fitsOI,cellsOI, o);
 
 end
