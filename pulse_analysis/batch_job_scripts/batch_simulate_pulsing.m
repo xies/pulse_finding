2d8ece8a69a97de7d24cd87c3b5a2641e6847452
xies <xies@mit.edu>
2014-06-18 13:44:58 -0400

    batch simulation submitter

--- pulse_analysis/batch_job_scripts/batch_simulate_pulsing.m
+++ pulse_analysis/batch_job_scripts/batch_simulate_pulsing.m
@@ -1,13 +1,13 @@
 %BATCH_SIMULATE_PULSING
 % To be used from a master shell script for use in batch jobs.
 
-function batch_simulate_pulsing(INPUT_MAT_FILE,iterID)
+function batch_simulate_pulsing(INPUT_MAT_FILE,OUT_FILENAME)
 
 addpath(genpath('~/Code'))
 
 [DIR_STR,name,ext] = fileparts(INPUT_MAT_FILE);
 
-if ~strcmpi(ext,'mat'),
+if ~strcmpi(ext,'.mat'),
 	error('Needs a .mat file as input.');
 end
 
@@ -18,8 +18,11 @@ load(INPUT_MAT_FILE);
 [f,nC] = estimate_simulation_params(fits,cells);
 
 disp('Performing pulsing simulation...');
+tic
 [fits_bs,cells_bs] = ...
 	fits.simulate_pulsing(cells,f);
+T = toc;
+disp(['Simulation finished in ' num2str(T) ' seconds'])
 
 fitsOI = fits_bs.get_embryoID(1:5);
 cellsOI = cells_bs.get_embryoID(1:5);
@@ -37,15 +40,18 @@ neighbor_defition.spatial.def = 'identity';
 
 fitsOI = fitsOI.find_near_fits(cellsOI,neighbor_defition);
 
-o.Nboot = 50;
+o.Nboot = 100;
 o.timewindows = time_windows;
 o.neighbor_def = neighbor_defition;
 o.monte_carlo = 'permute';
 o.filter = 'on';
 
-o.savepath = [DIR_STR]
+o.savepath = [OUT_FILENAME]
 
 disp('Permutation analysis...')
+tic;
 MC = monte_carlo_pulse_location(fitsOI,cellsOI, o);
+T = toc;
+disp(['Permutation analysis finished in ' num2str(T) ' seconds'])
 
 end
