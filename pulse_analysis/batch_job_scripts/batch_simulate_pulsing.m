1b21d334e6e0151f8f5a1545fd8142eeec28c24b
xies <xies@mit.edu>
2014-06-22 14:38:48 -0400

    Fixed batch job

--- pulse_analysis/batch_job_scripts/batch_simulate_pulsing.m
+++ pulse_analysis/batch_job_scripts/batch_simulate_pulsing.m
@@ -22,22 +22,13 @@ tic
 T = toc;
 disp(['Simulation finished in ' num2str(T) ' seconds'])
 
-fitsOI = fits_bs.get_embryoID(1:5);
-cellsOI = cells_bs.get_embryoID(1:5);
-
-[fits_bs,cells_bs] = fitsOI.simulate_pulsing(cellsOI,f,nC);
-
-fitsOI = fits_bs.get_embryoID(1:5);
-cellsOI = cells_bs.get_embryoID(1:5);
-
 time_windows = 10:10:100; % seconds
 
 neighbor_defition.temporal.def = @(time_diff,tau) (time_diff < tau & time_diff > 0);
 neighbor_defition.temporal.windows = time_windows;
 neighbor_defition.spatial.def = 'identity';
 
-fitsOI = fitsOI.find_near_fits(cellsOI,neighbor_defition);
-
+fitsOI = fits_bs.find_near_fits(cells_bs,neighbor_defition);
 o.Nboot = 100;
 o.timewindows = time_windows;
 o.neighbor_def = neighbor_defition;
@@ -48,7 +39,7 @@ o.savepath = [OUT_FILENAME]
 
 disp('Permutation analysis...')
 tic;
-MC = monte_carlo_pulse_location(fitsOI,cellsOI, o);
+MC = monte_carlo_pulse_location(fitsOI,cells_bs, o);
 T = toc;
 disp(['Permutation analysis finished in ' num2str(T) ' seconds'])
 
