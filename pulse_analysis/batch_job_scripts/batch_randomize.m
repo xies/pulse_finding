ce153bef194ddab3be02995b041001d0f77a59b6
xies <xies@mit.edu>
2014-07-08 16:49:24 -0400

    blah

--- pulse_analysis/batch_job_scripts/batch_randomize.m
+++ pulse_analysis/batch_job_scripts/batch_randomize.m
@@ -12,9 +12,24 @@ end
 disp(['Loading input pulse file: ' INPUT_MAT_FILE]);
 load(INPUT_MAT_FILE);
 
+time_windows = 10:10:100; % seconds
+
+neighbor_definition.temporal.def = @(time_diff,tau) (time_diff < tau & time_diff > 0);
+neighbor_definition.temporal.windows = time_windows;
+neighbor_definition.spatial.def = 'identity';
+
+fits = fits.find_near_fits(cells,neighbor_definition);
+o.Nboot = 100;
+o.timewindows = time_windows;
+o.neighbor_def = neighbor_definition;
+o.monte_carlo = 'simulation';
+o.filter = 'off';
+
+o.savepath = [OUT_FILENAME]
+
 if strcmpi(txtfile,'on')
 
-    fileID = fopen([OUT_FILENAME 'config.txt']);
+    fileID = fopen([OUT_FILENAME 'config.txt'],'w');
     
     % print embryoIDs
     embIDs = unique([fits.embryoID]);
@@ -30,21 +45,6 @@ if strcmpi(txtfile,'on')
     fclose(fileID);
     
 end
-
-time_windows = 10:10:100; % seconds
-
-neighbor_defition.temporal.def = @(time_diff,tau) (time_diff < tau & time_diff > 0);
-neighbor_defition.temporal.windows = time_windows;
-neighbor_defition.spatial.def = 'identity';
-
-fits = fits.find_near_fits(cells,neighbor_defition);
-o.Nboot = 100;
-o.timewindows = time_windows;
-o.neighbor_def = neighbor_defition;
-o.monte_carlo = 'simulation';
-o.filter = 'off';
-
-o.savepath = [OUT_FILENAME]
 
 disp('Randomization analysis...')
 MC = monte_carlo_pulse_location(fits,cells, o);
