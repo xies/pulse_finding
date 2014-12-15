3a56015a13587d2e0c396da8e9a3f1913370d9ab
xies <xies@mit.edu>
2014-12-15 17:01:09 -0500

    Added dt for corrected_time

--- spatiotemporal_correlation/ggplot_polyseries.R
+++ spatiotemporal_correlation/ggplot_polyseries.R
@@ -8,9 +8,9 @@ colnames(wt)[4] = 'behavior'; colnames(twist)[4] = 'behavior';
 colnames(wt)[5] = 'amplitude'; colnames(twist)[5] = 'amplitude';
 # colnames(wt)[6] = 'persistence'; colnames(twist)[6] = 'persistence';
 
-pulses = data.frame(near=wt$near,cr=wt$cr,behavior=wt$behavior,range=wt$range,
+pulses = data.frame(near=wt$near,cr=wt$cr/7.27,behavior=wt$behavior,range=wt$range,
                 genotype='wt',amplitude=wt$amplitude)
-pulses = rbind(pulses, data.frame(near=twist$near,cr=twist$cr,behavior=twist$behavior,range=twist$range,
+pulses = rbind(pulses, data.frame(near=twist$near,cr=twist$cr/7.27,behavior=twist$behavior,range=twist$range,
                           genotype='twist',amplitude=twist$amplitude) )
 
 ###
@@ -80,20 +80,16 @@ p
 ###
 
 df = pulses;
-df$near = df$near;
-
 stat_sum_df <- function(fun, geom="crossbar", ...) {
   stat_summary(fun.data=fun, colour="red", geom=geom, width=0.2, ...)
 }
 
-p = ggplot(data = subset(df, behavior < 3 & amplitude == 10 & genotype == 'wt'),
+p = ggplot(data = subset(df, amplitude == 10 & behavior < 3),
            aes(x=near, y = cr) )
-           
 p = p + geom_point(size = 5, color = 'blue')
 p = p + stat_sum_df( mean_cl_boot , geom = 'smooth',conf.int = 0.95)
-
-p = p + facet_grid( behavior  ~ . )
-p = p + coord_cartesian(ylim=c(0, 7.5))
+p = p + facet_grid( genotype  ~ . )
+p = p + coord_cartesian(ylim=c(0, 1))
 p = p + scale_x_continuous(breaks = seq(0 , 10, 1))
 p = p + theme(axis.text = element_text(size=20),
               title = element_text(size=20),
@@ -102,4 +98,3 @@ p = p + theme(axis.text = element_text(size=20),
               legend.title = element_text(size = 24)
               )
 p
-
